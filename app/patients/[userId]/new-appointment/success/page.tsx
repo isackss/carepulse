2a2f9a356bf4a7226ced0a804ce05dfd17e57285
Isacks <eddisacks@gmail.com>
2024-07-19 01:25:57 -0500

    appointment finished

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -2,7 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Success = () => {
+import { Button } from "@/components/ui/button";
+import { Doctors } from "@/constants";
+import { getAppointment } from "@/lib/actions/appointment.actions";
+import { formatDateTime } from "@/lib/utils";
+
+const Success = async ({
+  params: { userId },
+  searchParams,
+}: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || "";
+  const appointment = await getAppointment(appointmentId);
+  const doctor = Doctors.find(
+    (doc) => doc.name === appointment.primaryPhysician
+  );
+  console.log(doctor);
+  console.log(appointmentId);
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
       <div className="success-img">
@@ -32,9 +47,33 @@ const Success = () => {
         <section className="request-details">
           <p>Requested appointment details:</p>
           <div className="flex items-center gap-3">
-            {/* <Image src="/assets/"/> */}
+            <Image
+              src={doctor?.image!}
+              height={100}
+              width={100}
+              alt="doctor"
+              className="size-6"
+            />
+            <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+          </div>
+          <div className="flex gap-2">
+            <Image
+              src="/assets/icons/calendar.svg"
+              height={24}
+              width={24}
+              alt="calendar"
+              className=""
+            />
+            <p>{formatDateTime(appointment.schedule).dateTime}</p>
           </div>
         </section>
+
+        <Button variant="outline" className="shad-primary-btn">
+          <Link href={`/patients/${userId}/new-appointment`}>
+            New Appointment
+          </Link>
+        </Button>
+        <p className="copyright">© 2024 Care Pulse</p>
       </div>
     </div>
   );
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -26,10 +26,16 @@ export const createAppointment = async (
   }
 };
 
-/* export const getAppointment = async (appointentId: string) => {
+export const getAppointment = async (appointmentId: string) => {
   try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
 
+    return parseStringify(appointment);
   } catch (error) {
     console.log(error);
   }
-}; */
+};
